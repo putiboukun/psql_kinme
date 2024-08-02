83100a80bc82377b524c212d5fe6707002b9417d
Obito_od <94419003+putiboukun@users.noreply.github.com>
2024-08-02 20:45:27 +0900

    Update 1_create_db.sql

--- database/docker-entrypoint-initdb.d/1_create_db.sql
+++ database/docker-entrypoint-initdb.d/1_create_db.sql
@@ -1,9 +1,44 @@
 -- select 'create database trading' where not exists (select from pg_database where datname='trading');
 -- \c trading
 
-create table if not exists users (
-  id serial not null
-  , name character varying not null
-  , constraint users_PKC primary key (id)
-) ;
+CREATE TABLE tag (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
 
+CREATE TABLE node (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE "user" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30),
+    password VARCHAR(255)
+);
+
+CREATE TABLE workflow (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    workflow TEXT,
+    created_time TIMESTAMP,
+    updated_time TIMESTAMP,
+    user_id INTEGER,
+    content TEXT NOT NULL,
+    rendered_content TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user" (id)
+);
+
+CREATE TABLE workflows_tags (
+    workflow_id INTEGER,
+    tag_id INTEGER,
+    FOREIGN KEY (workflow_id) REFERENCES workflow (id),
+    FOREIGN KEY (tag_id) REFERENCES tag (id)
+);
+
+CREATE TABLE workflows_nodes (
+    workflow_id INTEGER,
+    node_id INTEGER,
+    FOREIGN KEY (workflow_id) REFERENCES workflow (id),
+    FOREIGN KEY (node_id) REFERENCES node (id)
+);
