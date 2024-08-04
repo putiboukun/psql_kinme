675b5648c4f7c08f2659031f88ba0a4c874559c8
Obito_od <94419003+putiboukun@users.noreply.github.com>
2024-08-04 10:34:24 +0900

    Delete database/1_create_db.sql

--- database/1_create_db.sql
+++
@@ -1,9 +0,0 @@
--- select 'create database trading' where not exists (select from pg_database where datname='trading');
--- \c trading
-
-create table if not exists users (
-  id serial not null
-  , name character varying not null
-  , constraint users_PKC primary key (id)
-) ;
-
